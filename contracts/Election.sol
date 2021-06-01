0b3c74df08d59f9814ff57521cb3341b8c550bcd
alien0525 <aman.ahmed.n.s@gmail.com>
2021-06-01 14:19:45 +0530

    cast votes

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -13,15 +13,34 @@ contract Election {
         addCandidate("Bharatiya Janata Party (BJP)");
         addCandidate("Indian National Congress (INC)");
         addCandidate("All India Trinamool Congress (TMC)");
+        addCandidate("None Of The Above (NOTA)");
     }
 
     mapping(uint => Candidate) public candidates ;
 
     uint public candidatesCount;
+
+    // Store accounts that have voted
+    mapping(address => bool) public voters;
     
     function addCandidate (string memory _name) private {
         candidatesCount ++;
         candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
+    function vote (uint _candidateId) public {
+        // require that they haven't voted before
+        // makes sure voters vote only once
+        require(!voters[msg.sender]);
+
+        // require a valid candidate
+        require(_candidateId > 0 && _candidateId <= candidatesCount);
+
+        // record that voter has voted
+        voters[msg.sender] = true;
+
+        // update candidate vote Count
+        candidates[_candidateId].voteCount++;
     }
 
 
